e01c3e65c1ed3c811c88413b30da24d0751387c3
victor braz <victor.90braz@gmail.com>
2023-10-30 17:39:39 +0100

    error message

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,13 @@
     use App\Http\Controllers\SessionController;
     use Illuminate\Support\Facades\Route;
     use MailchimpMarketing\ApiClient;
+use PhpParser\Node\Stmt\TryCatch;
 
-    Route::get('/ping', function () {
+    Route::post('/newsletter', function () {
+
+        request()->validate([
+            'email' => 'required|email'
+        ]);
 
         $mainchimp = new ApiClient();
         $mainchimp->setConfig([
@@ -15,12 +20,22 @@
             'server' => 'us21',
         ]);
 
-        $response = $mainchimp->lists->addListMember("76cf69a4f6", [
-            'email_address' => 'maria@gmail.com',
-            'status' => 'subscribed'
-        ]);
+        try {
 
-        dd($response);
+            $mainchimp->lists->addListMember("76cf69a4f6", [
+                'email_address' => request('email'),
+                'status' => 'subscribed'
+            ]);
+
+           return redirect('/')->with('success', 'You are now signed up for our newsletter!');
+
+        } catch(\Exception $error) {
+
+            throw \Illuminate\Validation\ValidationException::withMessages([
+                'email' => 'This email could not be added to our newsletter list.'
+            ]);
+        }
+
     });
 
     Route::get('/', [PostController::class, 'index'])->name('home');
