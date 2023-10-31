fc45a31f6a32a4b2835ad6ff382ddca82ce1fcd6
victor braz <victor.90braz@gmail.com>
2023-10-31 09:26:24 +0100

    refactor newsletter

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,14 @@
 
             (new Newsletter())->subscribe(request('email'));
 
-            return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-
         } catch(\Exception $error) {
 
             throw \Illuminate\Validation\ValidationException::withMessages([
                 'email' => 'This email could not be added to our newsletter list.'
             ]);
         }
+
+        return redirect('/')->with('success', 'You are now signed up for our newsletter!');
 
     });
 
