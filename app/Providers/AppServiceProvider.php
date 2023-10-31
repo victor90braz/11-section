af3b53d1654f36df31cf64f2203d451b607e646c
victor braz <victor.90braz@gmail.com>
2023-10-31 11:38:27 +0100

    Newsletter Subscription System

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
             ]);
 
             return new MailchimpNewsletter($client);
-            // return new ConvertKitNewsletter($client);
+            // return new ConvertKitNewsletter();
 
         });
     }
