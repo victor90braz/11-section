935a10df08f749a5542f8bd814a4dcf406c958de
victor braz <victor.90braz@gmail.com>
2023-10-31 09:52:53 +0100

    function client()

--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -10,16 +10,17 @@ class Newsletter {
 
         $list ??= config('services.mailchimp.lists.subscribers');
 
-        $mainchimp = new ApiClient();
-
-        $mainchimp->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            'server' => 'us21',
-        ]);
-
-        return $mainchimp->lists->addListMember( $list , [
+        return $this->client()->lists->addListMember( $list , [
             'email_address' => $email,
             'status' => 'subscribed'
+        ]);
+    }
+
+    protected function client() {
+
+        return (new ApiClient())->setConfig([
+            'apiKey' => config('services.mailchimp.key'),
+            'server' => 'us21',
         ]);
     }
 }
