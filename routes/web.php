9722fc86c6b448ede074785a9d6446c87c3309fc
victor braz <victor.90braz@gmail.com>
2023-10-31 10:37:25 +0100

    UPDATED

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@
     use App\Http\Controllers\SessionController;
     use Illuminate\Support\Facades\Route;
 
-
-
     Route::get('/', [PostController::class, 'index'])->name('home');
     Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
